aea8882af6158fdc63d6a9aeca30b98b26e92b00
Jonathan Thurman <jthurman@newrelic.com>
2019-08-26 15:57:33 -0500

    feat: timeRange, attribute selection, user limit selection

--- nerdlets/network-telemetry-overview/index.jsx
+++ nerdlets/network-telemetry-overview/index.jsx
@@ -1,18 +1,22 @@
 import PropTypes from "prop-types";
 import React from "react";
 import ChordDiagram from "react-chord-diagram";
-import { NerdGraphQuery } from "nr1";
+import { Dropdown, DropdownItem, NerdGraphQuery, Spinner } from "nr1";
+import * as d3 from "d3";
 
-const OverviewFacetQuery = `
-query ($accountid:Int!) {
+const NERDGRAPH_NRQL_QUERY = `
+query ($accountid:Int!, $nrql:Nrql!) {
   actor {
     account(id: $accountid) {
-      nrql(query: "FROM sflow SELECT sum(scaledByteCount) FACET networkSourceAddress, networkDestinationAddress LIMIT 150") {
+      nrql(query: $nrql, timeout: 10) {
         results
       }
     }
   }
 }`;
+
+const COLOR_START = "#11A893";
+const COLOR_END = "#FFC400";
 
 export default class MyNerdlet extends React.Component {
   static propTypes = {
@@ -27,14 +31,41 @@ export default class MyNerdlet extends React.Component {
 
     this.state = {
       entities: [],
+      isLoading: true,
+      queryAttribute: "throughput",
+      queryLimit: 50,
       relationships: [],
     };
   }
 
-  async componentDidMount() {
+  componentDidMount() {
+    this.fetchChordData();
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    const { timeRange } = this.props.launcherUrlState;
+    const { queryAttribute, queryLimit } = this.state;
+
+    if (
+      queryAttribute !== prevState.queryAttribute ||
+      queryLimit !== prevState.queryLimit ||
+      timeRange !== (prevProps.launcherUrlState || {}).timeRange
+    ) {
+      this.fetchChordData();
+    }
+  }
+
+  /*
+   * fetch data
+   */
+  async fetchChordData() {
+    this.setState({ isLoading: true });
     const results = await NerdGraphQuery.query({
-      query: OverviewFacetQuery,
-      variables: { accountid: 1 },
+      query: NERDGRAPH_NRQL_QUERY,
+      variables: {
+        accountid: 1,
+        nrql: this.createNrqlQuery(),
+      },
     });
 
     let entities = [];
@@ -58,15 +89,18 @@ export default class MyNerdlet extends React.Component {
       if (!Array.isArray(acc[s])) {
         acc[s] = [];
       }
-      acc[s][t] = d["sum.scaledByteCount"];
+      acc[s][t] = d["value"];
 
       return acc;
     }, []);
 
     const entityCount = entities.length;
+    const colorFunc = this.colorForEntity(entityCount);
 
+    let entityColors = [];
     let relationships = [];
     for (let y = 0; y < entityCount; y++) {
+      entityColors.push(colorFunc(y));
       relationships[y] = [];
       for (let x = 0; x < entityCount; x++) {
         relationships[y][x] = (data[y] || [])[x] || 0;
@@ -75,21 +109,107 @@ export default class MyNerdlet extends React.Component {
 
     this.setState({
       entities,
+      entityColors,
+      isLoading: false,
       relationships,
     });
   }
 
-  render() {
-    const { entities, relationships } = this.state;
+  colorForEntity(entityCount) {
+    return d3.scale
+      .linear()
+      .domain([1, entityCount])
+      .interpolate(d3.interpolateHsl)
+      .range([d3.rgb(COLOR_START), d3.rgb(COLOR_END)]);
+  }
+
+  createNrqlQuery() {
+    const { queryAttribute, queryLimit } = this.state;
+
+    let attr = "sum(scaledByteCount)";
+    if (queryAttribute === "count") {
+      attr = "count(*)";
+    }
 
     return (
-      <ChordDiagram
-        matrix={relationships}
-        componentId={1}
-        groupLabels={entities}
-        //groupColors={["#000000", "#FFDD89", "#957244", "#F26223"]}
-        width={1000}
-      />
+      "FROM sflow" +
+      " SELECT " +
+      attr +
+      " as 'value'" +
+      " FACET networkSourceAddress, networkDestinationAddress" +
+      " LIMIT " +
+      queryLimit +
+      " " +
+      this.timeRangeToNrql()
+    );
+  }
+
+  handleAttributeChange(attr) {
+    if (attr === "count" || attr === "throughput") {
+      this.setState({ queryAttribute: attr });
+    }
+  }
+
+  handleLimitChange(limit) {
+    if (limit > 0 && limit <= 100) {
+      this.setState({ queryLimit: limit });
+    }
+  }
+
+  /*
+   * Helper function to turn timeRange into NRQL Since
+   */
+  timeRangeToNrql() {
+    const { timeRange } = this.props.launcherUrlState || {};
+
+    if (!timeRange) {
+      return "";
+    } else if (timeRange.begin_time && timeRange.end_time) {
+      return ` SINCE ${timeRange.begin_time} UNTIL ${timeRange.end_time}`;
+    } else if (timeRange.duration) {
+      return ` SINCE ${timeRange.duration / 1000} SECONDS AGO`;
+    }
+
+    return "";
+  }
+
+  render() {
+    const { entities, entityColors, isLoading, relationships } = this.state;
+    const width = 1000;
+    const height = 700;
+    const outerRadius = Math.min(height, width) * 0.5 - 100;
+    const innerRadius = outerRadius - 10;
+
+    return (
+      <div>
+        <Dropdown title='Line Visualization'>
+          <DropdownItem onClick={() => this.handleAttributeChange("throughput")}>
+            Throughput
+          </DropdownItem>
+          <DropdownItem onClick={() => this.handleAttributeChange("count")}>
+            Flow Count
+          </DropdownItem>
+        </Dropdown>
+        <Dropdown title='Entity Limit'>
+          <DropdownItem onClick={() => this.handleLimitChange(25)}>25</DropdownItem>
+          <DropdownItem onClick={() => this.handleLimitChange(50)}>50</DropdownItem>
+          <DropdownItem onClick={() => this.handleLimitChange(100)}>100</DropdownItem>
+        </Dropdown>
+        {isLoading ? (
+          <Spinner />
+        ) : (
+          <ChordDiagram
+            componentId={1}
+            groupColors={entityColors}
+            groupLabels={entities}
+            height={height}
+            innerRadius={innerRadius}
+            matrix={relationships}
+            outerRadius={outerRadius}
+            width={width}
+          />
+        )}
+      </div>
     );
   }
 }
